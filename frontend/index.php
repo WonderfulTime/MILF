ba817762eb3b34349b8236bf943396c1e1a514b0
ShinkoWhat <34277360+ShinkoWhat@users.noreply.github.com>
2019-01-11 08:35:30 +1000

    main page update

--- frontend/index.php
+++ frontend/index.php
@@ -9,6 +9,39 @@ require $_SERVER['DOCUMENT_ROOT']."\backend/libs/rb.php";
 
   <br/><a href ="http://Quote/backend/verification_data/logout.php"> Разлогин </a>
 
+  <html lang="en">
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MILF</title>
+  </head>
+  <body>
+    <!-- <script src="addElement.js" async></script> -->
+    <div class="page-content">
+      <div class="header">
+        Quote
+      </div>
+      <div class="content-body">
+        <div class="post-container">
+          <div class="post">
+            some text about that one thing that happened last day aadsfjsdfjnsdsdffffffff
+          </div>
+          <div class="post">
+            some text
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script language="php">     include('http://Quote/frontend/includes/db_rb.php'); // подключение файла     </script>
+    <!-- <script src="index.js" type="text/javascript"></script> -->
+  </body>
+  </html>
+
 
 
 
@@ -21,22 +54,3 @@ require $_SERVER['DOCUMENT_ROOT']."\backend/libs/rb.php";
 <?php endif;
                                                 //проверка на наличие пользователя !конец!
 ?>
-<html lang="en">
-<head>
-  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-  <link rel="stylesheet" href="style.css">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>MILF</title>
-</head>
-<body>
-  <script src="addElement.js" async></script>
-  <div id="orig-div">hi1</div>
-  <button id="add-elem-button" onclick="addElement()">+</button>
-
-
-  <script language="php">     include('http://Quote/frontend/includes/db_rb.php'); // подключение файла     </script>
-  <script src="index.js" type="text/javascript"></script>
-</body>
-</html>
